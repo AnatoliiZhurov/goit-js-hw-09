0a9f0510ed47e73934ddadc539ad3cbdd10dbe29
AnatoliiZhurov <129226759+AnatoliiZhurov@users.noreply.github.com>
2023-09-09 20:45:18 +0300

    to pass

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+const elements = {
+  delayInput: document.querySelector(`input[name="delay"]`),
+  stepInput: document.querySelector(`input[name="step"]`),
+  amountInput: document.querySelector(`input[name="amount"]`),
+  form: document.querySelector(`.form`),
+};
+
+console.log(elements.delayInput.value);
+
+elements.form.addEventListener(`submit`, handlerSubmit);
+
+function handlerSubmit(event) {
+  event.preventDefault();
+  // const firstDelay = Number(elements.delayInput.value);
+  const step = Number(elements.stepInput.value);
+  const amount = Number(elements.amountInput.value);
+  let delay = Number(elements.delayInput.value);
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay);
+    delay += step;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  let promise = new Promise((res, rej) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        res({ position, delay });
+      } else {
+        rej({ position, delay });
+      }
+    }, delay);
+  });
+  promise
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
 }
